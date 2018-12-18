524101c427f8aa29cb092bebe930585922e345e2
babakoto <e.tokiniaina@gmail.com>
2018-12-18 01:22:38 +0100

    add methode GetMapping for more information

--- src/main/java/com/makijob/usermanager/controller/UserController.java
+++ src/main/java/com/makijob/usermanager/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 public class UserController {
 
@@ -32,7 +34,13 @@ public class UserController {
         userRepository.save(user);
     }
 
+    @GetMapping("/api/v1/user/more/{id}")
+    public Optional<User> moreInfo(@PathVariable Long id){
+        return userRepository.findById(id);
+    }
 
+
+   
 
 
 
